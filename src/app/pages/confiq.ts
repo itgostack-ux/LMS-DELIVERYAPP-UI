540ebab99cfbe04084cb125c3162b3eda0f63542
itgostack-ux <it.gostack@gmail.com>
2026-07-06 12:07:54 +0530

    Configure Firebase Hosting and update .gitignore

--- src/app/pages/confiq.ts
+++ src/app/pages/confiq.ts
@@ -3,8 +3,8 @@
 // Define constants or variables for configuration settings
 export const environment = {
   production: true, // Set to true for production environment
-//apiUrl: 'https://dotsmart-002-site5.gtempurl.com/api', // API URL
-   apiUrl: 'https://localhost:7055/api',
+  //apiUrl: 'https://dotsmart-002-site5.gtempurl.com/api', // API URL
+  apiUrl: 'https://localhost:7055/api',
   authApiUrl: 'https://dotsmart-002-site2.gtempurl.com/',
-  GofixUrl:'http://gofixapi.azurewebsites.net/'
+  GofixUrl: 'http://gofixapi.azurewebsites.net/'
 };
